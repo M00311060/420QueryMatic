56eaa599cf6b73ae8df5f0f4fc1cae2f1f0adeee
Jake DePriest <157439270+jakedballer@users.noreply.github.com>
2024-12-12 17:44:59 -0600

    Login Page Styling

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import HeaderLoginPage from '../Components/Headers/HeaderLoginPage';
 import FooterLoginpage from '../Components/Footers/FooterLoginPage';
+import './PagesStyle/LoginStyle.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -10,36 +11,45 @@ const Login = () => {
     const navigate = useNavigate();
 
     const universalPassword = 'QueryMatic24';
-    const murrayEmail = /^[a-zA-Z0-9._%+-]+@murraystate\.edu$/
+    const murrayEmail = /^[a-zA-Z0-9._%+-]+@murraystate\.edu$/;
 
     const handleLogin = () => {
         if (murrayEmail.test(email.toLowerCase()) && password === universalPassword) {
             navigate('/survey');
         } else {
-            alert('Error: Invalid Credentials')
+            alert('Error: Invalid Credentials');
         }
     };
 
     return (
-        <div> 
+        <div className="login-page">
             <HeaderLoginPage />
-            <img src ='src\assets\QuerymaticLogo.jpeg'
-                        alt = "QueryMatic Logo" />
-            <div className = "EmailTextBox">
+            <img
+                src='src/assets/QuerymaticLogo.jpeg'
+                alt="QueryMatic Logo"
+            />
+            <div className="EmailTextBox">
                 <label>Email:</label>
-                <input type ="email" value = {email} onChange = {(e) => setEmail(e.target.value)} />
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
 
-            <div className = "PasswordTextBox">
+            <div className="PasswordTextBox">
                 <label>Password:</label>
-                <input type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)} />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </div>
 
-            <button type = "submit" onClick = {handleLogin}>Login</button>
+            <button type="submit" onClick={handleLogin}>Login</button>
             <FooterLoginpage />
-
         </div>
     );
-}
+};
 
-export default Login
+export default Login;
